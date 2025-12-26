93ab505b52fc597dc2e42c37f462ec56ffafaa47
Derrell Turner <derrellturner@Derrells-MacBook-Air.local>
2025-12-25 18:53:27 -0600

    Added regions and vaguard data and testing correct output

--- LoreExplore/src/lore_explore.py
+++ LoreExplore/src/lore_explore.py
@@ -1,12 +1,20 @@
-import json #json 
+import json # loads json 
 #TKINTER
-#open them
 #def function
-#search them
-#display results
-with open('/Users/derrellturner/projects/LoreExplore/data/characters.json') as f:
-    data = json.load(f)
-#def()
+with open('LoreExplore/data/characters.json') as f: #with .... as f is good cause it does auto cleanup, safe and cleaner aka no random files floating around
+    chars = json.load(f) #json.load(f) converts json into a python object
+    #data is now a dictionary with a key 'characters' pointing to my list of dictionaries
+with open('LoreExplore/data/regions.json') as f: 
+     regs = json.load(f)
+with open('LoreExplore/data/vanguard.json') as f: 
+     vang = json.load(f)
 
-for character in data["characters"]:
+
+for character in chars["characters"]: #loops through each character in th elist and prints the name field
     print(character["name"])
+
+for region in regs["regions"]:
+    print(region["region_name"])
+
+for vanguard in vang["vanguards"]:
+    print(vanguard["vanguard_name"])
